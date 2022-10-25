f55a5854be6a0d442cb84fd469d5f16932192f79
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-10-25 11:08:46 -0300

    fix: Fixing storybook static path

--- storybook-static/assets/preview.e014ef2f.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.4753344b.js";import"./iframe.39112e5a.js";import"./es.map.constructor.50baec03.js";import"./es.number.to-fixed.ace5c9d4.js";import"./index.16865cbc.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.e014ef2f.js.map
